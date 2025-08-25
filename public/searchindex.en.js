8854a18ba5188f86bb5f79c4eb3c08fc91e69b0d
crystian <ti11@casajoka.com.br>
2025-08-25 11:19:47 -0300

    Adiciona workflow de deploy automático

---
+++ public/searchindex.en.js
@@ -0,0 +1,34 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Minha Base de Conhecimento",
+    "content": "Bem-vindo à nossa base de conhecimento!\nEste é o primeiro capítulo.\nPróximos Passos Instalação Configuração inicial",
+    "description": "Bem-vindo à nossa base de conhecimento!\nEste é o primeiro capítulo.\nPróximos Passos Instalação Configuração inicial",
+    "tags": [],
+    "title": "Como Começar",
+    "uri": "/crystian-kb/como-comecar/index.html"
+  },
+  {
+    "breadcrumb": "Minha Base de Conhecimento",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/crystian-kb/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Minha Base de Conhecimento",
+    "uri": "/crystian-kb/index.html"
+  },
+  {
+    "breadcrumb": "Minha Base de Conhecimento",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/crystian-kb/tags/index.html"
+  }
+]
